e22f4565e69d6a9278da3a8e1c18543bdd7ac1cb
Balaji Rajasekaran <tekybala@gmail.com>
2026-04-04 17:16:10 +1100

    added feedback form

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
 import { Header } from './Header';
 import { ControlDetailPanel } from '@/components/ControlDetailPanel';
+import { FeedbackWidget } from '@/components/FeedbackWidget';
 
 export function Layout() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -17,6 +18,7 @@ export function Layout() {
         </main>
       </div>
       <ControlDetailPanel />
+      <FeedbackWidget />
     </div>
   );
 }
